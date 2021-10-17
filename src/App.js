334ce823063ba828eefcd78fe143588992857452
ZohaibQaiserAhmad <30640101+ZohaibQaiserAhmad@users.noreply.github.com>
2021-10-17 03:48:34 -0400

    Question1
    React Quiz, creating basic list view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+//input given
+const rooms = [
+  { room_type: "Queen", vacant_rooms: 5, price: 100 },
+  { room_type: "Double", vacant_rooms: 3, price: 75 },
+  { room_type: "Twin", vacant_rooms: 8, price: 60 }
+];
+
+const RoomFunction = (props) => {
+
+  //wrap the lists in ordered list as required
+  //use of deconstructor to get each properties of out room
+  return <ol>
+    {props.rooms.map(({ room_type, vacant_rooms, price }, index) => {
+      //list the items as specified
+      return <li key={index}>{room_type}, {vacant_rooms}, ${price}</li>
+    })}
+  </ol>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RoomFunction rooms={rooms}></RoomFunction>
     </div>
   );
 }
